e94ba1f6f48da803aca19dfb449e71ccaf7a708a
sintiya18 <tiyaasintiya18@gmail.com>
2025-05-03 13:35:36 +0700

    penambahan foto,ganti font,menambah nama,nim,alamat

--- lib/hal_profil.dart
+++ lib/hal_profil.dart
@@ -7,41 +7,156 @@ class HalProfil extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Profil'),
+        title: const Text(
+          'My Profil',
+          style: TextStyle(
+            fontFamily: 'Comic_Neue',
+            fontWeight: FontWeight.bold, 
+            fontSize: 24, 
+          ),
+        ),
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 73, 245, 245),
+        backgroundColor: const Color.fromARGB(255, 153, 158, 158),
         automaticallyImplyLeading: false,
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            
             mainAxisSize: MainAxisSize.min,
-            children: const [
-              CircleAvatar(backgroundImage: AssetImage('assets/images/appp2.jpeg' ),radius: 50,),
-              Text(
-                'Email',
-              style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Comic_Neue'),
-                textAlign: TextAlign.center,
+            children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/appp2.jpeg'),
+                radius: 50,
               ),
-              SizedBox(height: 4),
-              Text(
-                'sintiya12@gmail.com',
-                style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Comic_Neue'),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Password',
-                style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Comic_Neue'),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 4),
-              Text(
-                '12345',
-                style: TextStyle(fontWeight: FontWeight.bold,fontFamily: 'Comic_Neue'),
-                textAlign: TextAlign.center,
+              const SizedBox(height: 20),
+
+           
+              Card(
+                elevation: 6,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8, 
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Nama :',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Sinntiya Ayu Setyowati',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            'NIM :',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            '220102045',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Alamat :',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Brubuh, Ngadirojo, Wonogiri',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Email :',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'sintiya12@gmail.com',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Align(
+                          alignment: Alignment.centerLeft, 
+                          child: const Text(
+                            'Password :',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            '12345',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Comic_Neue',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -50,5 +165,8 @@ class HalProfil extends StatelessWidget {
     );
   }
 }
+
+
+
 
 
